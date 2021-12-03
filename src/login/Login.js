5aa7f3e499821fa0f3edc5db2957a6273f38441d
Robyn Thomas <robyn.thomas@spandigital.com>
2021-12-03 23:36:21 +0200

    wip: tailwind setup

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, signInWithGoogle } from '../services/firebase';
+import { auth, signInWithGoogle } from "../services/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Login() {
-    const [user, loading] = useAuthState(auth);
-    const navigate = useNavigate();
-    useEffect(() => {
-      if (loading) return;
-      if (user) navigate("/");
-    }, [user, loading, navigate]);
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate("/");
+  }, [user, loading, navigate]);
 
-
-    return (
-        <div className="login_panel">
-
-            <div className="google-btn" onClick={signInWithGoogle}>
-                <p className="btn-text"><b>Sign in with google</b></p>
-            </div>
-        </div>
-        )
+  return (
+    <div className="login_panel">
+      <div className="google-btn" onClick={signInWithGoogle}>
+        <p className="btn-text underline">
+          <b>Sign in with google</b>
+        </p>
+      </div>
+    </div>
+  );
 }
