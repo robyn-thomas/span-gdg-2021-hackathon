f2abd672bbd9b5914fc187ce4e00ae103e3a65c1
Robyn Thomas <robyn.thomas@spandigital.com>
2021-12-04 13:28:07 +0200

    wip: styling

--- src/dashboard/Case.js
+++ src/dashboard/Case.js
@@ -1,10 +1,14 @@
 export default function Case(props) {
   return (
-    <div>
-      <p>
-        {' '}
-        <b>Link:</b> {props.data.link} <b>Status:</b> {props.data.status}{' '}
-      </p>
-    </div>
+    <li
+      key={props.link}
+      className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md"
+    >
+      <div>
+        <p>
+          <b>Link:</b> {props.data.link} <b>Status:</b> {props.data.status}
+        </p>
+      </div>
+    </li>
   );
 }
