a73070098c44fc3426c119561dbcf3b9c1aaa417
Robyn Thomas <robyn.thomas@spandigital.com>
2021-12-05 11:20:26 +0200

    wip: updates

--- src/login/Login.js
+++ src/login/Login.js
@@ -31,7 +31,7 @@ export default function Login() {
               <div className="google-btn" onClick={signInWithGoogle}>
                 <p
                   type="text"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="cursor-pointer w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   <b>Sign in with Google</b>
                 </p>
