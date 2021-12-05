f366aa220ec917bd6bc85859afa0b165948066b6
Jayan Smart <jayandrinsmart@gmail.com>
2021-12-05 11:46:27 +0200

    Routing fix

--- src/services/piii.js
+++ src/services/piii.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 export const addProfile = (userId, twitterId) => {
-    axios.get('localhost:8080/scan/twitter/'+userId+'/' + twitterId)  
+    axios.get('localhost:8080/twitter/add_profile/'+userId+'/' + twitterId)  
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    axios.get('localhost:8080/twitter/scan/'+userId+'/' + twitterId)  
     .then(function (response) {
         console.log(response);
     })
